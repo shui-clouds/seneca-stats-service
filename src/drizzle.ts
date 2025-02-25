c29faadaf0d4b4eb042d5b7342e3ba28b847cd3e
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2025-02-25 10:35:20 +0000

    update schema file name

--- src/drizzle.ts
+++ src/drizzle.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { Resource } from "sst";
-import * as schema from "./stats.sql";
+import * as schema from "./schema";
 
 const pool = new Pool({
   host: Resource.Postgres.host,
